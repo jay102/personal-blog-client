bceb04e7e46a9697eea038666f2e8f7bb07d38f1
James <jordunjames@gmail.com>
2019-11-13 12:55:49 +0100

    adding netlify form control

--- src/components/Frontend/Contact/ContactView.js
+++ src/components/Frontend/Contact/ContactView.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const ContactView = (props) => {
+  const { name, email, phone, message } = props;
   return (
     <React.Fragment>
       <header className="masthead" style={props.mystyle}>
@@ -20,32 +21,32 @@ const ContactView = (props) => {
         <div className="row">
           <div className="col-lg-8 col-md-10 mx-auto">
             <p>{props.text}</p>
-            <form name="contactform" id="contactForm" method="POST" noValidate data-netlify="true">
+            <form id="contactForm" noValidate onSubmit={props.handleSubmit}>
               <div className="control-group">
                 <div className="form-group floating-label-form-group controls">
                   <label>Name</label>
-                  <input type="text" className="form-control" placeholder="Name" id="name" required data-validation-required-message="Please enter your name." />
+                  <input type="text" className="form-control" placeholder="Name" name="name" onChange={props.handleChange} id="name" value={name} required data-validation-required-message="Please enter your name." />
                   <p className="help-block text-danger"></p>
                 </div>
               </div>
               <div className="control-group">
                 <div className="form-group floating-label-form-group controls">
                   <label>Email Address</label>
-                  <input type="email" className="form-control" placeholder="Email Address" id="email" required data-validation-required-message="Please enter your email address." />
+                  <input type="email" className="form-control" placeholder="Email Address" name="email" onChange={props.handleChange} value={email} id="email" required data-validation-required-message="Please enter your email address." />
                   <p className="help-block text-danger"></p>
                 </div>
               </div>
               <div className="control-group">
                 <div className="form-group col-xs-12 floating-label-form-group controls">
                   <label>Phone Number</label>
-                  <input type="tel" className="form-control" placeholder="Phone Number" id="phone" required data-validation-required-message="Please enter your phone number." />
+                  <input type="tel" className="form-control" placeholder="Phone Number" name="phone" onChange={props.handleChange} id="phone" value={phone} required data-validation-required-message="Please enter your phone number." />
                   <p className="help-block text-danger"></p>
                 </div>
               </div>
               <div className="control-group">
                 <div className="form-group floating-label-form-group controls">
                   <label>Message</label>
-                  <textarea rows="5" className="form-control" placeholder="Message" id="message" required data-validation-required-message="Please enter a message."></textarea>
+                  <textarea rows="5" className="form-control" placeholder="Message" name="message" onChange={props.handleChange} id="message" value={message} required data-validation-required-message="Please enter a message."></textarea>
                   <p className="help-block text-danger"></p>
                 </div>
               </div>
